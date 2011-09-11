595efacc5ae7415c23680685c13ff99f4b827922
Matt McCormick <mbmccormick@gmail.com>
2011-09-11 15:02:19 -0400

    Upgraded the instance size for web role. Added worker role for background data download.

--- BlobStorageEngine/WorkerRole.cs
+++ BlobStorageEngine/WorkerRole.cs
@@ -8,28 +8,117 @@ using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.StorageClient;
+using OneTransitAPI;
+using OneTransitAPI.Common;
+using OneTransitAPI.Data;
+using System.IO;
+using Ionic.Zip;
 
 namespace BlobStorageEngine
 {
     public class WorkerRole : RoleEntryPoint
     {
+        private CloudBlobContainer container;
+
         public override void Run()
         {
-            // This is a sample worker implementation. Replace with your logic.
-            Trace.WriteLine("BlobStorageEngine entry point called", "Information");
+            Trace.WriteLine("Initializing.", "Information");
+
+            WebClient web = new WebClient();
+            web.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.CacheIfAvailable);
 
             while (true)
             {
-                Thread.Sleep(10000);
-                Trace.WriteLine("Working", "Information");
+                Trace.WriteLine("Waking up...", "Information");
+
+                try
+                {
+                    DatabaseDataContext db = new DatabaseDataContext();
+                    var agencies = from a in db.Agencies where a.GTFSUrl != null orderby a.Name select a;
+
+                    foreach (Agency a in agencies)
+                    {
+                        Trace.WriteLine("Downloading new GTFS data for " + a.Name + " (" + a.AgencyID + ")...", "Information");
+
+                        ZipFile gtfsData = ZipFile.Read(web.OpenRead(a.GTFSUrl));
+
+                        foreach (ZipEntry file in gtfsData)
+                        {
+                            Trace.WriteLine("Saving " + file.FileName + " to storage.", "Information");
+
+                            CloudBlob blob = container.GetBlobReference(a.AgencyID.ToLower() + "/" + file.FileName.ToLower());
+
+                            MemoryStream output = new MemoryStream();
+                            file.Extract(output);
+
+                            blob.UploadFromStream(output);
+                        }
+
+                        Trace.WriteLine("Complete.", "Information");
+                    }
+                }
+                catch (StorageClientException e)
+                {
+                    Trace.TraceError("Exception when processing queue item. Message: '{0}'", e.Message);
+                    System.Threading.Thread.Sleep(5000);
+                }
+
+                Trace.WriteLine("Going to sleep.", "Information");
+                Thread.Sleep(1000 * 60 * 60 * 24 * 7); // execute every 7 days
             }
         }
 
         public override bool OnStart()
         {
-            ServicePointManager.DefaultConnectionLimit = 12;
+            DiagnosticMonitor.Start("DiagnosticsConnectionString");
 
             RoleEnvironment.Changing += RoleEnvironmentChanging;
+
+            CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) =>
+            {
+                try
+                {
+                    configSetter(RoleEnvironment.GetConfigurationSettingValue(configName));
+                }
+                catch (RoleEnvironmentException e)
+                {
+
+                    Trace.TraceError(e.Message);
+                    System.Threading.Thread.Sleep(5000);
+                }
+            });
+
+            var storageAccount = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+
+            CloudBlobClient blobStorage = storageAccount.CreateCloudBlobClient();
+            container = blobStorage.GetContainerReference("gtfs");
+
+            Trace.TraceInformation("Creating storage container...");
+
+            bool storageInitialized = false;
+            while (!storageInitialized)
+            {
+                try
+                {
+                    container.CreateIfNotExist();
+
+                    var permissions = container.GetPermissions();
+                    permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+                    container.SetPermissions(permissions);
+                }
+                catch (StorageClientException e)
+                {
+                    if (e.ErrorCode == StorageErrorCode.TransportError)
+                    {
+                        Trace.TraceError("Storage services initialization failure. Check your storage account configuration settings. If running locally, ensure that the Development Storage service is running. Message: '{0}'", e.Message);
+                        System.Threading.Thread.Sleep(5000);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
 
             return base.OnStart();
         }
