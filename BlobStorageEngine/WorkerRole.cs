7eefc2b20500a576489f9f8bf0891208839d49aa
Matt McCormick <mbmccormick@gmail.com>
2011-09-11 11:56:25 -0400

    Began adding background worker code.

---
+++ BlobStorageEngine/WorkerRole.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace BlobStorageEngine
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        public override void Run()
+        {
+            // This is a sample worker implementation. Replace with your logic.
+            Trace.WriteLine("BlobStorageEngine entry point called", "Information");
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Trace.WriteLine("Working", "Information");
+            }
+        }
+
+        public override bool OnStart()
+        {
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            RoleEnvironment.Changing += RoleEnvironmentChanging;
+
+            return base.OnStart();
+        }
+
+        private void RoleEnvironmentChanging(object sender, RoleEnvironmentChangingEventArgs e)
+        {
+            if (e.Changes.Any(change => change is RoleEnvironmentConfigurationSettingChange))
+                e.Cancel = true;
+        }
+    }
+}
